ff1d2356e537555a305087714b74938c61f25db6
undercure <111484351+undercure@users.noreply.github.com>
2022-08-18 16:54:49 +0800

    Add files via upload

---
+++ docs/lt/voucher/static/js/BaseFunction.js
@@ -0,0 +1,21 @@
+function confirm() {
+    const a = document.getElementById("riqi");
+    console.dir(a);
+    const b = document.getElementById("date");
+    console.dir(b);
+    b.innerHTML=a.value;
+    const c = document.getElementById("dingdanbianhao");
+    const d = document.getElementById("order number");
+    console.dir(c);
+    console.dir(d);
+    d.innerHTML=c.value;
+    const e = document.getElementById("shangrenbianma");
+    const f = document.getElementById("Merchant number");
+    console.dir(e);
+    console.dir(f);
+    f.innerHTML=e.value;
+
+}
+function changOut(obj){
+    document.getElementById("out").innerHTML=obj.value;
+}
