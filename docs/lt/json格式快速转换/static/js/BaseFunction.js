37cb957f0c1540b051740b23c0fde96df1671663
WORK-NUC <undercure@qq.com>
2022-09-13 17:47:03 +0800

    消除最后一组server_port会undefined的bug

--- docs/lt/json格式快速转换/static/js/BaseFunction.js
+++ docs/lt/json格式快速转换/static/js/BaseFunction.js
@@ -1,39 +1,38 @@
 function confirm() {
 	document.getElementById("JsonResult").innerHTML = "";
-	var myObj = JSON.parse(document.getElementById("text").value) ;
+	const myObj = JSON.parse(document.getElementById("text").value);
 	document.getElementById("JsonResult").innerHTML += "{\n\t\"configs\" \: \[";
 	for (i = 0; i < myObj.data.length; i++) {
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\{";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"AdditionalRoute\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"ConnectedTimes\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Download_traffic\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"FailureTimes\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Latency\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Upload_traffic\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"charge_type\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"enable\": true,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"group\": \"myServer\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"iplocation\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"ipregion\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"method\": \"none\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"obfs\": \"plain\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"obfsparam\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"password\": \"" + document.getElementById("password").value + "\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"protocol\": \"origin\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"protocolparam\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"remarks\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server\"\:\"" + myObj.data[i].ip + "\"\," ;
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server_port\"\:"+ myObj.data[i].port + "\," ;
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server_udp_port\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"type\": 5,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"udp_over_tcp\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"username\": \"" + document.getElementById("username").value + "\"";
-		if( i == myObj.data.length -1 ){
+		document.getElementById("JsonResult").innerHTML +=	( "\n\t\t\{"
+																	+ "\n\t\t\t\"AdditionalRoute\": \"\","
+																	+ "\n\t\t\t\"ConnectedTimes\": 0,"
+																	+ "\n\t\t\t\"Download_traffic\": 0,"
+																	+ "\n\t\t\t\"FailureTimes\": 0,"
+																	+ "\n\t\t\t\"Latency\": 0,"
+																	+ "\n\t\t\t\"Upload_traffic\": 0,"
+																	+ "\n\t\t\t\"charge_type\": 0,"
+																	+ "\n\t\t\t\"enable\": true,"
+																	+ "\n\t\t\t\"group\": \"myServer\","
+																	+ "\n\t\t\t\"iplocation\": \"\","
+																	+ "\n\t\t\t\"ipregion\": \"\","
+																	+ "\n\t\t\t\"method\": \"none\","
+																	+ "\n\t\t\t\"obfs\": \"plain\","
+																	+ "\n\t\t\t\"obfsparam\": \"\","
+																	+ "\n\t\t\t\"password\": \"" + document.getElementById("password").value + "\","
+																	+ "\n\t\t\t\"protocol\": \"origin\","
+																	+ "\n\t\t\t\"protocolparam\": \"\","
+																	+ "\n\t\t\t\"remarks\": \"\","
+																	+ "\n\t\t\t\"server\"\:\"" + myObj.data[i].ip + "\"\,"
+																	+ "\n\t\t\t\"server_port\"\:"+ myObj.data[i].port + "\,"
+																	+ "\n\t\t\t\"server_udp_port\": 0,"
+																	+ "\n\t\t\t\"type\": 5,"
+																	+ "\n\t\t\t\"udp_over_tcp\": 0,"
+																	+ "\n\t\t\t\"username\": \"" + document.getElementById("username").value + "\"");
+		if( i === myObj.data.length -1 ){
 			document.getElementById("JsonResult").innerHTML += "\n\t\t\}";
 		}else{
 			document.getElementById("JsonResult").innerHTML += "\n\t\t\},";
 		}
 	}
-	document.getElementById("JsonResult").innerHTML += "\n\t\]," + "\t\"idInUse\": 0";
-	document.getElementById("JsonResult").innerHTML += "\n\}";
+	document.getElementById("JsonResult").innerHTML += "\n\t\]," + "\n\t\"idInUse\": 0" + "\n\}";
 }
--- docs/lt/proxylist.json/static/js/BaseFunction.js
+++ docs/lt/proxylist.json/static/js/BaseFunction.js
@@ -2,50 +2,51 @@ function confirmServerPort() {
 	//清空文本框内容
 	document.getElementById("IPAdrressWithServerPort").innerHTML = "";
 	//按行获取源数据
-	var IPAdrr = document.getElementById("IPAdrress").value.split('\n');
-	for (i = 0; i < IPAdrr.length; i++) {
-		document.getElementById("IPAdrressWithServerPort").innerHTML += IPAdrr[i] + "\:" + document.getElementById("ServerPort").value +"\n";
+	const IPAdrr = document.getElementById("IPAdrress").value.split('\n');
+	for (let i = 0; i < IPAdrr.length; i++) {
+		document.getElementById("IPAdrressWithServerPort").innerHTML += IPAdrr[i] + "\:" + document.getElementById("ServerPort").value ;
+		if( i !== IPAdrr.length -1){
+			document.getElementById("IPAdrressWithServerPort").innerHTML +="\n";
+		}
 	}
 }
 function confirmJson() {
-	var IPServer = document.getElementById("IPAdrressWithServerPort").value.split('\n');
+	const IPServer = document.getElementById("IPAdrressWithServerPort").value.split('\n');
 	//清空文本框内容
 	document.getElementById("JsonResult").innerHTML = "";
 	//生成json文件
 	document.getElementById("JsonResult").innerHTML += "{\n\t\"configs\" \: \[";
-	for (i = 0; i < IPServer.length; i++) {
-		var temp = IPServer[i].split('\:');
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\{";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"AdditionalRoute\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"ConnectedTimes\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Download_traffic\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"FailureTimes\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Latency\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"Upload_traffic\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"charge_type\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"enable\": true,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"group\": \"myServer\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"iplocation\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"ipregion\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"method\": \"none\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"obfs\": \"plain\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"obfsparam\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"password\": \"" + document.getElementById("password").value + "\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"protocol\": \"origin\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"protocolparam\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"remarks\": \"\",";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server\"\:\"" + temp[0].value + "\"\," ;
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server_port\"\:"+ temp[1] + "\," ;
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"server_udp_port\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"type\": 5,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"udp_over_tcp\": 0,";
-		document.getElementById("JsonResult").innerHTML += "\n\t\t\t\"username\": \"" + document.getElementById("username").value + "\"";
-		if( i == IPServer.length -1 ){
-			document.getElementById("JsonResult").innerHTML += "\n\t\t\}";
-		}else{
-			document.getElementById("JsonResult").innerHTML += "\n\t\t\},";		
+	for (let i = 0; i < IPServer.length; i++) {
+		const temp = IPServer[i].split('\:');
+		document.getElementById("JsonResult").innerHTML += ( "\n\t\t\{"
+																	+ "\n\t\t\t\"AdditionalRoute\": \"\","
+																	+ "\n\t\t\t\"ConnectedTimes\": 0,"
+																	+ "\n\t\t\t\"Download_traffic\": 0,"
+																	+ "\n\t\t\t\"FailureTimes\": 0,"
+																	+ "\n\t\t\t\"Latency\": 0,"
+																	+ "\n\t\t\t\"Upload_traffic\": 0,"
+																	+ "\n\t\t\t\"charge_type\": 0,"
+																	+ "\n\t\t\t\"enable\": true,"
+																	+ "\n\t\t\t\"group\": \"myServer\","
+																	+ "\n\t\t\t\"iplocation\": \"\","
+																	+ "\n\t\t\t\"ipregion\": \"\","
+																	+ "\n\t\t\t\"method\": \"none\","
+																	+ "\n\t\t\t\"obfs\": \"plain\","
+																	+ "\n\t\t\t\"obfsparam\": \"\","
+																	+ "\n\t\t\t\"password\": \"" + document.getElementById("password").value + "\","
+																	+ "\n\t\t\t\"protocol\": \"origin\","
+																	+ "\n\t\t\t\"protocolparam\": \"\","
+																	+ "\n\t\t\t\"remarks\": \"\","
+																	+ "\n\t\t\t\"server\"\:\"" + temp[0] + "\"\,"
+																	+ "\n\t\t\t\"server_port\"\:"+ temp[1] + "\,"
+																	+ "\n\t\t\t\"server_udp_port\": 0,"
+																	+ "\n\t\t\t\"type\": 5,"
+																	+ "\n\t\t\t\"udp_over_tcp\": 0,"
+																	+ "\n\t\t\t\"username\": \"" + document.getElementById("username").value + "\"")
+																	+ "\n\t\t\}";
+		if( i !== IPServer.length -1 ){
+			document.getElementById("JsonResult").innerHTML += ",";
 		}
 	}
-	document.getElementById("JsonResult").innerHTML += "\n\t\]," + "\n\t\"idInUse\": 0";
-	document.getElementById("JsonResult").innerHTML += "\n\}";
+	document.getElementById("JsonResult").innerHTML += "\n\t\],\n\t\"idInUse\": 0\n\}";
 }
